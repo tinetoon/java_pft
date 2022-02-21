fb425727106f0d1beb2064b416cd28611ee6fad6
Дюжаков Антон <tinetoon@mail.ru>
2022-02-21 13:52:40 +0300

    Добавлен запускатель для Gradle

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,9 +1,7 @@
-package java;
-
 public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        System.out.println("Привет мир!!!");
+        System.out.println("Hello World!!!");
     }
 }
