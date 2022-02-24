1b79c0a249c6606755f094709541169c8649b58d
Дюжаков Антон <tinetoon@mail.ru>
2022-02-24 13:52:30 +0300

    Задание № 4 - Добавлен тест на создание новой записи в БД

---
+++ addressbook_web_test/src/test/java/ru/stqa/pft/addressbook/testData/NewNoteData.java
@@ -0,0 +1,62 @@
+package ru.stqa.pft.addressbook.testData;
+
+public class NewNoteData {
+    private final String FIRST_NAME_XPATH = "firstname";
+    private final String LAST_NAME_XPATH = "lastname";
+    private final String ADDRESS_XPATH = "address";
+    private final String EMAIL_XPATH = "email";
+    private final String MOBILE_PHONE_XPATH = "mobile";
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String mobileFone;
+
+    public NewNoteData(String firstName, String lastName, String address, String email, String mobilePhone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.mobileFone = mobilePhone;
+    }
+
+    public String getFIRST_NAME_XPATH() {
+        return FIRST_NAME_XPATH;
+    }
+
+    public String getLAST_NAME_XPATH() {
+        return LAST_NAME_XPATH;
+    }
+
+    public String getADDRESS_XPATH() {
+        return ADDRESS_XPATH;
+    }
+
+    public String getEMAIL_XPATH() {
+        return EMAIL_XPATH;
+    }
+
+    public String getMOBILE_PHONE_XPATH() {
+        return MOBILE_PHONE_XPATH;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMobileFone() {
+        return mobileFone;
+    }
+}
